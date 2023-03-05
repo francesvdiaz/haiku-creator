1fafdd702d4a0d3a7f0ed565919c650412048e88
vfrances.ww@gmail.com <115190000+francesvdiaz@users.noreply.github.com>
2023-03-05 14:33:42 -0800

    pass first test, haiku object being created

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -2,10 +2,10 @@ import Haiku from './../src/js/Haiku.js';
 
 describe('Haiku', () => {
   test('should correctly create a haiku object with 4 lines', () => {
-    const Haiku = new Haiku 
-    expect(Haiku.line1).toEqual(0);
-    expect(Haiku.line2).toEqual(0);
-    expect(Haiku.line3).toEqual(0);
-    expect(Haiku.line4).toEqual(0);
+    const newHaiku = new Haiku(0,0,0,0);
+    expect(newHaiku.line1).toEqual(0);
+    expect(newHaiku.line2).toEqual(0);
+    expect(newHaiku.line3).toEqual(0);
+    expect(newHaiku.line4).toEqual(0);
   });
 });
---
+++ src/js/haiku.js
@@ -0,0 +1,8 @@
+export default class Haiku {
+  constructor (line1,line2,line3,line4) {
+    this.line1 = line1;
+    this.line2 = line2;
+    this.line3 = line3;
+    this.line4 = line4;
+  }
+}
